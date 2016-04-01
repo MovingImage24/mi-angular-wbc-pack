257a7bf32e7cb5aed23946d722acfdddfb08d89f
Enrico Hoffmann <dasrick@gmail.com>
2016-04-01 15:34:42 +0200

    add LESS into build pipeline and dist code

--- src/template/index.js
+++ src/template/index.js
@@ -6,6 +6,7 @@
 module.exports = angular
   .module('mi.WbcPack.tpls', [
     require('./messaging-view').name,
-    require('./1col-centerbox').name
+    require('./1col-centerbox').name,
+    require('./login').name
   ])
 ;
---
+++ src/template/login/index.js
@@ -0,0 +1,23 @@
+'use strict';
+var tplName = 'mi-angular-wbc-pack/template/login.html';
+module.exports = angular.module(tplName, [])
+  .run(['$templateCache', function ($templateCache) {
+    $templateCache.put(tplName,
+      '<form class="form" id="formLogin" name="formLogin" novalidate>' +
+      '<h3 translate="security.form.login.header"></h3>' +
+      '<p translate="security.form.login.msg"></p>' +
+      '<div class="form-group" ng-class="{\'has-error\':formLogin.email.$invalid && !formLogin.email.$pristine }">' +
+    '<input type="email" name="email" class="form-control" placeholder="{{\'security.form.email.placeholder\'|translate}}" ng-model="loginVm.loginData.email" autofocus required>' +
+    '</div>' +
+    '<div class="form-group" ng-class="{\'has-error\':formLogin.password.$invalid && !formLogin.password.$pristine }">' +
+    '<input type="password" name="password" class="form-control" placeholder="{{\'security.form.password.placeholder\'|translate}}" ng-model="loginVm.loginData.password" required>' +
+    '</div>' +
+    '<div class="row">' +
+    '<div class="col-xs-12">' +
+    '<button ng-click="loginVm.login()" ng-disabled="formLogin.$invalid" translate="security.form.btn.login.label" class="btn btn-block btn-primary"></button>' +
+    '</div>' +
+    '</div>' +
+    '</form>'
+    );
+  }])
+;
